dcd1f06b1d49b3fa1f90e3a5c6a8e2d685a527b3
ivana <ivana.djipalo.00@fesb.hr>
2021-06-10 00:29:39 +0200

    Creating store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
+import rootReducer from "./store/reducers/reducers";
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(ReduxThunk),
+    window.devToolsExtension ? window.devToolsExtension() : (f) => f
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
   </React.StrictMode>,
   document.getElementById("root")
 );
