173572792eabf7975c7e35d0be8d6377af390d94
ivana <ivana.djipalo.00@fesb.hr>
2021-06-10 00:38:32 +0200

    Adding login form

---
+++ src/pages/Login/LoginPage.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+
+import { useDispatch, useState } from "react-redux";
+import { login } from "../../store/actions/actions";
+
+const LoginPage = () => {
+  const dispatch = useDispatch();
+  const [error, setError] = useState(null);
+
+  return (
+    <div className="loginForm">
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={async (values) => {
+          try {
+            await dispatch(login(values.email, values.password));
+          } catch (err) {
+            setError(err.message);
+          }
+        }}
+      >
+        <Form className="form">
+          <div className="inputField">
+            <label htmlFor="email">Email</label>
+            <Field
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="inputField">
+            <label htmlFor="password">Password</label>
+            <Field
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default LoginPage;
